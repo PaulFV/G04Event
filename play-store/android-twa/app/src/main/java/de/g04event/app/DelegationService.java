7285e11be9c4c259b8692d5c06ebba95235d9c30
PaulFV <fodorpaul@web.de>
2026-09-13 02:09:30 +0200

    build: add Android TWA project

---
+++ play-store/android-twa/app/src/main/java/de/g04event/app/DelegationService.java
@@ -0,0 +1,14 @@
+package de.g04event.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
